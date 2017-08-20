7f3e44920a2f3284fa50733b887a02505a72c381
ShichaoMa <270515529@qq.com>
2017-08-20 13:59:35 +0800

    commit

---
+++ structor/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+from .redis_feed import RedisFeed
+from .check_status import main
+from .start_project import start as start_project
+
+VERSION = '0.8.0'
+
+AUTHOR = "cn"
+
+AUTHOR_EMAIL = "cnaafhvk@foxmail.com"
+
+URL = "https://www.github.com/ShichaoMa/structure_spider"
+
+
+def check():
+    main()
+
+
+def feed():
+    RedisFeed.parse_args().start()
